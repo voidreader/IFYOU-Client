43af8e46cf11b6707d6ba85ec6d5c6204f9d8047
Radiogaga.jin <radiogaga.jin@gmail.com>
2021-11-26 18:32:16 +0900

    으아아아아아 EpisodeData..!!

---
+++ Assets/0. Platform/B. Scripts/b. ScriptSystem/DataClass/EpisodeData.cs
@@ -0,0 +1,177 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+namespace PIERStory {
+    
+    
+    // * 매번 JSON에서 파싱해서 꺼내오는게 너무 귀찮아서 만들었다! 
+    
+    [Serializable]
+    public class EpisodeData
+    {
+        JsonData episodeJSON; // 에피소드 정보 
+        
+        JsonData purchaseData; // 에피소드 구매 정보
+        JsonData episodeGalleryImageJSON; // 에피소드에 등장하는 갤러리 이미지 JSON
+        
+        public string episodeID = string.Empty; // 에피소드 ID 
+        public string episodeNO = string.Empty; // 에피소드 순번
+        public string episodeType = string.Empty; // 에피소드 타입
+        public string episodeTitle = string.Empty; // 에피소드 타이틀
+        public string episodeSummary = string.Empty; // 에피소드 스토리 요약 
+        
+        public string endingType = string.Empty;  // 엔딩 타입 
+        public string dependEpisode = string.Empty;  // 의존 에피소드 
+        public bool endingOpen = false; // 엔딩 오픈 여부 
+        public float totalSceneCount = 0f;         // 진행률(분모)
+        public float playedSceneCount = 0f;        // 플레이어 진행률(분자)
+        public float sceneProgressorValue = 0; // 씬 프로그레서 값 
+        
+        public float episodeGalleryImageProgressValue = 0; // 일러스트 프로그레서 값 
+        
+        public string squareImageURL = string.Empty; // 목록 사각 썸네일 
+        public string squareImageKey = string.Empty; 
+        
+        public string popupImageURL = string.Empty; // 팝업 이미지 
+        public string popupImageKey = string.Empty; 
+        
+        public EpisodeState episodeState = EpisodeState.Future; // 에피소드 플레이 상태 
+        public PurchaseState purchaseState = PurchaseState.None; // 구매 상태 
+        
+        public int pricePremiumSale = 0; // 프리미엄 세일 가격
+        public int pricePremium = 0; // 프리미엄 가격
+        
+        public string currencyOneTime = string.Empty; // 1회 플레이 화폐
+        public int priceOneTime = 0; // 1회 플레이 가격
+        
+        
+        
+        public EpisodeData(JsonData __j) {
+            episodeJSON = __j;
+            
+            InitData();
+        }
+        
+        /// <summary>
+        /// 데이터 설정하기
+        /// </summary>
+        void InitData() {
+            episodeID = SystemManager.GetJsonNodeString(episodeJSON, "episode_id");
+            episodeNO = SystemManager.GetJsonNodeString(episodeJSON, "episode_no");
+            episodeType = SystemManager.GetJsonNodeString(episodeJSON, "episode_type");
+            episodeTitle = SystemManager.GetJsonNodeString(episodeJSON, "title");
+            episodeSummary = SystemManager.GetJsonNodeString(episodeJSON, "summary");
+            
+            endingOpen = SystemManager.GetJsonNodeBool(episodeJSON, "ending_open");
+            endingType = SystemManager.GetJsonNodeString(episodeJSON, "ending_type");
+            dependEpisode = SystemManager.GetJsonNodeString(episodeJSON, "depend_episode");
+            
+            // 에피소드에 등장한는 갤러리 이미지
+            episodeGalleryImageJSON = episodeJSON["galleryImage"];
+            episodeGalleryImageProgressValue = UserManager.main.CalcGalleryImage(episodeJSON); // Progressor용 값 
+            
+            // * 이미지 
+            squareImageURL = SystemManager.GetJsonNodeString(episodeJSON, LobbyConst.TITLE_IMAGE_URL);
+            squareImageKey = SystemManager.GetJsonNodeString(episodeJSON, LobbyConst.TITLE_IMAGE_KEY);
+            popupImageURL = SystemManager.GetJsonNodeString(episodeJSON, LobbyConst.POPUP_IMAGE_URL);
+            popupImageKey = SystemManager.GetJsonNodeString(episodeJSON, LobbyConst.POPUP_IMAGE_KEY);
+            
+            
+            // * 가격
+            pricePremiumSale = int.Parse(SystemManager.GetJsonNodeString(episodeJSON, LobbyConst.EPISODE_SALE_PRICE));
+            pricePremium = int.Parse(SystemManager.GetJsonNodeString(episodeJSON, LobbyConst.EPISODE_PRICE));
+            currencyOneTime = SystemManager.GetJsonNodeString(episodeJSON, "one_currency");
+            priceOneTime = int.Parse(SystemManager.GetJsonNodeString(episodeJSON, "one_price"));
+            
+            
+            // * 사건 진행율 
+            totalSceneCount = int.Parse(episodeJSON["total_scene_count"].ToString()); // 에피소드에 등장하는 모든 사건ID 카운트
+            playedSceneCount = int.Parse(episodeJSON["played_scene_count"].ToString()); // 유저가 한번이라도 플레이 했던 사건 ID 카운트 
+             if(totalSceneCount > 0)        
+                sceneProgressorValue = playedSceneCount / totalSceneCount;
+                
+                
+                
+            SetEpisodePlayState();
+            SetPurchaseState();
+        }
+        
+        /// <summary>
+        /// 에피소드의 플레이 상태 설정 
+        /// </summary>
+        void SetEpisodePlayState() {
+            string currentRegularEpisodeID = string.Empty;
+            
+            if(episodeType == "side") {
+                episodeState = EpisodeState.Current;
+                return;
+            }
+            
+            
+            // 아직 작업중인 작품은 값이 없을 수 있다.
+            if(UserManager.main.GetUserProjectRegularEpisodeCurrent() == null) {
+                currentRegularEpisodeID = string.Empty;
+            }
+            else {
+                currentRegularEpisodeID = UserManager.main.GetUserProjectRegularEpisodeCurrent()["episode_id"].ToString();
+            }
+            
+            
+            // current의 ID와 현재 에피소드 ID가 같음. 
+            if(currentRegularEpisodeID == episodeID) {
+                episodeState = EpisodeState.Current;
+            }
+            else { // 다른 경우에 과거, 미래 체크 
+            
+                // episode Progress 테이블에 있음 
+                if (UserManager.main.CheckEpisodeProgress(episodeID)) {
+                    episodeState = EpisodeState.Prev; // 과거 
+                }    
+                else { // 없으면 미래.
+                    episodeState = EpisodeState.Future; // 미래 
+                }
+            }
+        }
+        
+        /// <summary>
+        /// 에피소드의 구매 상태 설정 
+        /// </summary>        
+        void SetPurchaseState() {
+            string episodePurchaseState = string.Empty;
+            
+            // 구매내역을 purchaseDate로 out 
+            // * purchaseState 정한다. 
+            if (UserManager.main.CheckPurchaseEpisode(episodeID, ref purchaseData))
+            {
+                Debug.Log(JsonMapper.ToStringUnicode(purchaseData));
+                
+                if(purchaseData["purchase_type"].ToString() == "Permanent")
+                {
+                    purchaseState = PurchaseState.Permanent; // 영구적인 구매 상태 
+
+                    // 구매 했어도 가격이 원래 무료면 FREE 처리
+                    if(pricePremiumSale < 1)
+                        purchaseState = PurchaseState.Free;
+                }
+                else if (purchaseData["purchase_type"].ToString() == "OneTime"){ // OneTime
+                    purchaseState = PurchaseState.OneTime;
+                }
+                else if (purchaseData["purchase_type"].ToString() == "Rent"){ // 
+                    purchaseState = PurchaseState.Rent;
+                }
+            }
+            else
+            {
+                // 구매 내역이 없는 경우에 대한 처리 
+                if(pricePremiumSale < 1)
+                    purchaseState = PurchaseState.Free; // 무료!
+                
+            }
+        } // ? SetPurchaseState
+        
+    }   
+
+}

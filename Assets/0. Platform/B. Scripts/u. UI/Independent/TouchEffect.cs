5c00483b26168ed4866c44e639fcf4feab93a9ee
unknown <38553075+gatemap@users.noreply.github.com>
2021-12-13 21:08:23 +0900

    Add touch effect
    인게임 진입 로딩화면에서 터치 시, 텍스트 전환 되도록 하는 함수도 추가함

---
+++ Assets/0. Platform/B. Scripts/u. UI/Independent/TouchEffect.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace PIERStory
+{
+    public class TouchEffect : MonoBehaviour
+    {
+        public GameObject touchEffect;
+        GameObject effect;
+        Vector3 touchInPos = Vector3.forward;
+        Vector3 touchOutPos = Vector3.back;
+
+        private void Update()
+        {
+            if(Application.isEditor)
+            {
+                if (Input.GetMouseButtonDown(0))
+                    touchInPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                if(Input.GetMouseButtonUp(0))
+                {
+                    touchOutPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    CheckOutPosition();
+                }
+            }
+            else
+            {
+                if (Input.touchCount < 1)
+                    return;
+
+                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                    touchInPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+
+                if(Input.GetTouch(0).phase == TouchPhase.Ended)
+                {
+                    touchOutPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                    CheckOutPosition();
+                }
+            }
+        }
+
+        void CheckOutPosition()
+        {
+            if (touchInPos.x >= touchOutPos.x - 0.4f && touchInPos.x <= touchOutPos.x + 0.4f &&
+                touchInPos.y >= touchOutPos.y - 0.4f && touchInPos.y <= touchOutPos.y + 0.4f)
+                CreateTouchEffect(touchOutPos);
+        }
+
+        void CreateTouchEffect(Vector3 touchPos)
+        {
+            Vector3 touchedPos = touchPos;
+            touchInPos = Vector3.forward;
+            touchOutPos = Vector3.back;
+            touchedPos = new Vector3(touchedPos.x, touchedPos.y, 0);
+            effect = Instantiate(touchEffect, touchedPos, Quaternion.identity);
+
+            Destroy(effect, 0.5f);
+        }
+    }
+}
--- Assets/0. Platform/B. Scripts/u. UI/Views/Game/ViewLoadingGame.cs
+++ Assets/0. Platform/B. Scripts/u. UI/Views/Game/ViewLoadingGame.cs
@@ -1,13 +1,12 @@
 ﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 using TMPro;
 
 namespace PIERStory
 {
-    public class ViewLoadingGame :CommonView
+    public class ViewLoadingGame :CommonView, IPointerClickHandler
     {
         public ImageRequireDownload loadingImage;
 
@@ -15,6 +14,8 @@ namespace PIERStory
         public TextMeshProUGUI textInfo;
 
         int loadingTextIndex = 0;
+
+        const string KEY_LOADING_TEXT = "loading_text";
 
         public override void OnStartView()
         {
@@ -29,7 +30,7 @@ namespace PIERStory
 
         }
 
-         IEnumerator RoutineGameLoading()
+        IEnumerator RoutineGameLoading()
         {
             // 게임 매니저에서 에피소드 스크립트 정보 가져올 때까지 대기
             yield return new WaitUntil(() => GameManager.main.isScriptFetch);
@@ -39,15 +40,29 @@ namespace PIERStory
             else
                 loadingImage.SetDownloadURL(string.Empty, string.Empty);
 
-            if(GameManager.main.loadingDetailJson.Count >0)
+            if (GameManager.main.loadingDetailJson.Count > 0)
             {
                 loadingTextIndex = 0;
-                textInfo.text = SystemManager.GetJsonNodeString(GameManager.main.loadingDetailJson[loadingTextIndex], "loading_text");
+                textInfo.text = SystemManager.GetJsonNodeString(GameManager.main.loadingDetailJson[loadingTextIndex], KEY_LOADING_TEXT);
             }
 
             yield return new WaitUntil(() => GameManager.main.GetCurrentPageInitialized());
 
             Doozy.Runtime.Signals.Signal.Send(LobbyConst.STREAM_GAME, "gameLoadingComplete", string.Empty);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            // 아직 jsonData가 없는 경우 터치해도 아무 동작하지 않도록 한다
+            if (GameManager.main.loadingDetailJson == null || GameManager.main.loadingDetailJson.Count == 0)
+                return;
+
+            loadingTextIndex++;
+
+            if (loadingTextIndex == GameManager.main.loadingDetailJson.Count)
+                loadingTextIndex = 0;
+
+            textInfo.text = SystemManager.GetJsonNodeString(GameManager.main.loadingDetailJson[loadingTextIndex], KEY_LOADING_TEXT);
         }
     }
 }

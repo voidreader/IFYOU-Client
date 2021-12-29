23dc93adcfa692cf90a7eb9b20561d3cfa2cb4d9
Radiogaga.jin <pi-er@pi-er.com>
2021-12-29 16:34:40 +0900

    commit

---
+++ Assets/0. Platform/B. Scripts/u. UI/PopUp/PopupPass.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PIERStory {
+    public class PopupPass : PopupBase
+    {
+        [SerializeField] PassBanner passBanner;
+        
+        
+        public override void Show()
+        {
+            base.Show();
+        }
+    }
+}

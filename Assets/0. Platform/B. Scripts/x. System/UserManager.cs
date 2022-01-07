168de03a703f2501cf87b36e091f1d19cec5e0d9
gatemap <38553075+gatemap@users.noreply.github.com>
2022-01-07 19:59:36 +0900

    Rollback

--- Assets/0. Platform/B. Scripts/x. System/UserManager.cs
+++ Assets/0. Platform/B. Scripts/x. System/UserManager.cs
@@ -1866,8 +1866,10 @@ namespace PIERStory
         public bool CheckSceneHistory(string __sceneID)
         {
             // 어드민 유저 무조건 true 
+            /*
             if(CheckAdminUser())
                 return true; 
+            */
             
             
             if (currentStoryJson == null || GetNodeProjectSceneHistory() == null)

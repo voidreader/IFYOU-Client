aa4dc41ff27e10eeca377000059d04fedce1cd40
gatemap <38553075+gatemap@users.noreply.github.com>
2022-02-18 15:53:30 +0900

    터치스크린이펙트
    InputManager에서 Input system으로 변경

--- Assets/0. Platform/B. Scripts/u. UI/Independent/TouchEffect.cs
+++ Assets/0. Platform/B. Scripts/u. UI/Independent/TouchEffect.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace PIERStory
 {
@@ -6,54 +7,26 @@ namespace PIERStory
     {
         public GameObject touchEffect;
         GameObject effect;
-        Vector3 touchInPos = Vector3.one;
+        Vector2 touchInPos = Vector2.one;
         Vector3 touchOutPos = Vector3.one;
 
-        private void Update()
+
+        public void OnTouchScreen(InputAction.CallbackContext context)
         {
-            if(Application.isEditor)
+            if(context.action.phase == InputActionPhase.Canceled)
             {
-                if (Input.GetMouseButtonDown(0))
-                    touchInPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                touchOutPos = Camera.main.ScreenToWorldPoint(touchInPos);
+                touchOutPos = new Vector3(touchOutPos.x, touchOutPos.y, 0f);
 
-                if(Input.GetMouseButtonUp(0))
-                {
-                    touchOutPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                    CheckOutPosition();
-                }
-            }
-            else
-            {
-                if (Input.touchCount < 1)
-                    return;
-
-                if (Input.GetTouch(0).phase == TouchPhase.Began)
-                    touchInPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-
-                if(Input.GetTouch(0).phase == TouchPhase.Ended)
-                {
-                    touchOutPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-                    CheckOutPosition();
-                }
+                effect = Instantiate(touchEffect, touchOutPos, Quaternion.identity);
+                Destroy(effect, 1f);
             }
         }
 
-        void CheckOutPosition()
+        public void OnTouchPosition(InputAction.CallbackContext context)
         {
-            if (touchInPos.x >= touchOutPos.x - 0.4f && touchInPos.x <= touchOutPos.x + 0.4f &&
-                touchInPos.y >= touchOutPos.y - 0.4f && touchInPos.y <= touchOutPos.y + 0.4f)
-                CreateTouchEffect(touchInPos);
-        }
-
-        void CreateTouchEffect(Vector3 touchPos)
-        {
-            Vector3 touchedPos = touchPos;
-            touchedPos = new Vector3(touchedPos.x, touchedPos.y, 0);
-            effect = Instantiate(touchEffect, touchedPos, Quaternion.identity);
-            
-            touchInPos = Vector3.one;
-            touchOutPos = Vector3.one;
-            Destroy(effect, 0.5f);
+            touchInPos = context.ReadValue<Vector2>();
         }
     }
 }
+

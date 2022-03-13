59470fee419eb0ae091a9507905af87fd8bc0565
Radiogaga.jin <pi-er@pi-er.com>
2022-03-13 23:28:42 +0900

    준비 작업

---
+++ Assets/0. Platform/B. Scripts/u. UI/UI Element/StoryLobby/EpisodeEndControls.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PIERStory {
+
+    public class EpisodeEndControls : StoryLobbyMain
+    {
+
+        protected override void Update() {
+            base.Update();
+        }
+        
+        new void InitStoryLobbyControls() {
+                        
+            InitBaseInfo(); // 기본정보
+
+            SetPlayState(); // 플레이 및 타이머 설정 
+        }
+        
+        /// <summary>
+        /// 거의 똑같은데 마지막만 다름 
+        /// </summary>
+        public void InitBaseInfo() {
+            textReduceWaitingTime.text = SystemManager.main.waitingReduceTimeAD.ToString() +" min"; // 광고보고 차감되는 시간 SysteManager..
+           
+            currentStoryData =  StoryManager.main.CurrentProject; // 현재 작품 
+            projectCurrentJSON = UserManager.main.GetUserProjectRegularEpisodeCurrent(); // 작품상에서 현재 위치 
+            
+            
+            // 에피소드 타이틀 초기화
+            SetEpisodeTitleText(string.Empty);
+            
+            if(projectCurrentJSON == null) {
+                SystemManager.ShowSimpleAlert("Invalid Episode State. Please contact to help center");
+                return;
+            }
+            
+            currentEpisodeID = SystemManager.GetJsonNodeString(projectCurrentJSON, "episode_id");
+            currentEpisodeData = StoryManager.GetRegularEpisodeByID(currentEpisodeID);
+            currentEpisodeData.SetPurchaseState(); // 구매기록 refresh.
+            
+            
+            hasPremium = UserManager.main.HasProjectFreepass();
+            isEpisodeContinuePlay = false;
+            
+        }
+      
+    }
+}

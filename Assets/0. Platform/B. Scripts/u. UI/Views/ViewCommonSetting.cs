1ce5fc70c192bb373a022f97b002720ea137a01e
한가을 <nodes_@naver.com>
2022-07-26 08:52:35 +0900

    메뉴를 제작진 변경하고 다국어 비활성화 처리.

--- Assets/0. Platform/B. Scripts/u. UI/Views/ViewCommonSetting.cs
+++ Assets/0. Platform/B. Scripts/u. UI/Views/ViewCommonSetting.cs
@@ -48,7 +48,7 @@ namespace PIERStory {
             Signal.Send(LobbyConst.STREAM_TOP, LobbyConst.TOP_SIGNAL_SHOW_PROPERTY_GROUP, false, string.Empty);
             Signal.Send(LobbyConst.STREAM_TOP, LobbyConst.TOP_SIGNAL_SHOW_BACK_BUTTON, true, string.Empty);
             Signal.Send(LobbyConst.STREAM_TOP, LobbyConst.TOP_SIGNAL_VIEW_NAME_EXIST, true, string.Empty);
-            Signal.Send(LobbyConst.STREAM_TOP, LobbyConst.TOP_SIGNAL_VIEW_NAME, SystemManager.GetLocalizedText("8001"), string.Empty);
+            Signal.Send(LobbyConst.STREAM_TOP, LobbyConst.TOP_SIGNAL_VIEW_NAME, SystemManager.GetLocalizedText("8015"), string.Empty);
             Signal.Send(LobbyConst.STREAM_TOP, LobbyConst.TOP_SIGNAL_ATTENDANCE, false, string.Empty);               
             
         }
